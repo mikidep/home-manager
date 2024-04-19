325f69236f4bc8eb0494b5493939e774fadea0fe
Michele De Pascalis <michele.de.pascalis.1024@gmail.com>
2024-04-19 15:26:35 +0300

    Added abbreviations

--- shell.nix
+++ shell.nix
@@ -10,15 +10,19 @@
     functions = {
       nix_run = ''nix run nixpkgs#$argv[1] -- $argv[2..]'';
     };
-    interactiveShellInit = ''
+    interactiveShellInit = let
+      neovim-cmd = "nix run ~/dotfiles/neovim --";
+    in ''
       set fish_greeting # Disable greeting
       fish_add_path .local/bin/
       # zoxide init fish | source
       abbr --add sg "${pkgs.shell_gpt}/bin/sgpt --repl temp --shell"
       abbr --add ns --set-cursor "nix shell nixpkgs#%"
       abbr --add nix-list --set-cursor 'find $(nix build nixpkgs#% --print-out-paths --no-link) -print0 | ${pkgs.nnn}/bin/nnn'
-      abbr --add nr "nix_run"
-      set EDITOR nvim
+      abbr --add nr --set-cursor "nix run nixpkgs#%"
+      abbr --add nvim "${neovim-cmd}"
+      abbr --add hm "home-manager --flake ~/dotfiles/home-manager"
+      set EDITOR "${neovim-cmd}"
     '';
     plugins = [
       {
