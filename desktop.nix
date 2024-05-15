28c98d3171af0399d2346606cd60514f84ec69e1
Michele De Pascalis <michele.de.pascalis.1024@gmail.com>
2024-05-15 19:19:13 +0300

    wl-clipboard

--- desktop.nix
+++ desktop.nix
@@ -27,7 +27,7 @@ in {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [fcitx5-gtk libsForQt5.fcitx5-qt];
   };
-
+  home.packages = with pkgs; [wl-clipboard];
   programs.firefox = {
     enable = true;
     profiles.default = {
