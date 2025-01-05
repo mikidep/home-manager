17b7dcf294e449dc888026de48d0a8a5c4319e16
Michele De Pascalis <michele.de.pascalis.1024@gmail.com>
2025-01-05 15:43:57 +0100

    ttu vpn

--- vpn.nix
+++ vpn.nix
@@ -1,3 +1,18 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [openfortivpn openfortivpn-webview];
+  home.packages = with pkgs; [
+    (writeShellApplication {
+      name = "ttuvpn";
+      runtimeInputs = with pkgs; [
+        openfortivpn
+        openfortivpn-webview
+      ];
+      text = let
+        vpnaddr = "vpn.taltech.ee:443";
+        trcert = "dd7b28bce7714f20c2afb556e72d95e0bf3d10951eacecfd7aef4c00e00520fc";
+      in ''
+        openfortivpn-webview ${vpnaddr} 2>/dev/null \
+        | sudo openfortivpn ${vpnaddr} --cookie-on-stdin --pppd-accept-remote --trusted-cert ${trcert}
+      '';
+    })
+  ];
 }
